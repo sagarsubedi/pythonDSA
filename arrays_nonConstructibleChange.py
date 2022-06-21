c6b41223a169692c9e208dab544ccabc58c7ab5f
Sagar Subedi <sagar@example.com>
2022-06-21 14:27:51 -0500

    solved nonConstructibleChange problem

---
+++ arrays_nonConstructibleChange.py
@@ -0,0 +1,23 @@
+"""
+  Given an array of positive integers representing the values of coins in your
+  possession, write a function that returns the minimum amount of change (the
+  minimum sum of money) that you cannot create. The given coins can have
+  any positive integer value and aren't necessarily unique (i.e., you can have
+  multiple coins of the same value).
+"""
+
+
+def nonConstructibleChange(coins):
+
+    coins.sort()
+
+    currentChangeCreated = 0
+    for coin in coins:
+        if coin > currentChangeCreated + 1:
+            return currentChangeCreated + 1
+
+        currentChangeCreated += coin
+
+    return currentChangeCreated + 1
+
+# O(nlogn) time | O(1) space (since coins.sort() is in-place sorting)
